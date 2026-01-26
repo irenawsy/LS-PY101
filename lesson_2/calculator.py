ac93144b544fec990e13c4115a09310e0b30a665
Irena <wsy.irena@gmail.com>
2026-01-26 15:42:49 +0000

    Assignments and example attempts up still 2.15

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,13 +1,40 @@
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f"==> {message}")
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+# Welcome the user
+prompt('Welcome to Calculator!')
 
 # Ask user for first number
-num_1 = input('First number: ')
+prompt("What's the first number?")
+num_1 = input()
+
+while invalid_number(num_1):
+    prompt("Hmm... that does not look like a valid number.")
+    num_1 = input()
 
 # Ask user for second number
-num_2 = input('Second number: ')
+prompt("What's the second number?")
+num_2 = input()
+
+while invalid_number(num_2):
+    prompt("Hmm... that does not look like a valid number.")
+    num_1 = input()
 
 # Ask user for type of operation to perform: + - x /
-op_type = input('Specify operation type (+, -, x, /): ')
+prompt('Specify operation type (+, -, *, /): ')
+op_type = input()
+
+while op_type not in ["+", "-", "*", "/"]:
+    prompt('You must choose +, -, * /')
+    op_type = input()
 
 num_1 = int(num_1)
 num_2 = int(num_2)
@@ -20,7 +47,7 @@ match op_type:
     case '-':
         result = num_1 - num_2
 
-    case 'x':
+    case '*':
         result = num_1 * num_2
 
     case '/':
