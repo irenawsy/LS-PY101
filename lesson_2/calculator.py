757c14c76c29b97a508b2a35c8400cf637e1d4eb
Irena <wsy.irena@gmail.com>
2026-01-28 13:18:01 +0000

    Added ZeroDivisionError handling method.

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,53 +1,68 @@
 import json
 
-# Load messages from JSON file
-with open('calculator_messages.json', 'r') as file:
-    MESSAGES = json.load(file)
+# SUB-FUNCTIONS
+def prompt(prompt_message):
+    print(f"==> {prompt_message}")
 
-def prompt(message):
-    print(f"==> {message}")
+def message(msg_key, lang = 'en'):
+    return MESSAGES[lang][msg_key]
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
 
     return False
 
-prompt("Select language")
+# PROGRAM CODE
+# Load messages from JSON file
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
+
+# Supported languages
+supported_languages = ["en", "es"]
+
+# Ask user for preferred language
+prompt("Select language (en - English, es - Español)")
 user_lang = input()
 
+while user_lang not in supported_languages:
+    prompt(message('invalid_language'))
+    user_lang = input()
+
+# Actual Calculator
 while True:
     # Welcome the user
-    prompt(MESSAGES[user_lang]['welcome'])
+    prompt(message('welcome', user_lang))
 
     # Ask user for first number
-    prompt(MESSAGES[user_lang]['ask_num1'])
+    prompt(message('ask_num1', user_lang))
     num_1 = input()
 
     while invalid_number(num_1):
-        prompt(MESSAGES[user_lang]['invalid_number'])
+        prompt(message('invalid_number', user_lang))
         num_1 = input()
 
     # Ask user for second number
-    prompt(MESSAGES[user_lang]['ask_num2'])
+    prompt(message('ask_num2', user_lang))
     num_2 = input()
 
     while invalid_number(num_2):
-        prompt(MESSAGES[user_lang]['invalid_number'])
-        num_1 = input()
+        prompt(message('invalid_number', user_lang))
+        num_2 = input()
 
     # Ask user for type of operation to perform: + - x /
-    prompt(MESSAGES[user_lang]['ask_operation'])
+    prompt(message('ask_operation', user_lang))
     op_type = input()
 
     while op_type not in ["+", "-", "*", "/"]:
-        prompt(MESSAGES[user_lang]['invalid_operation'])
+        prompt(message('invalid_operation', user_lang))
         op_type = input()
 
-    num_1 = int(num_1)
-    num_2 = int(num_2)
+    # Convert input into appropriate data type
+    num_1 = float(num_1)
+    num_2 = float(num_2)
 
     # Calculate
     match op_type:
@@ -61,13 +76,23 @@ while True:
             result = num_1 * num_2
 
         case '/':
-            result = num_1 / num_2
+            try:
+                result = num_1 / num_2
+            except ZeroDivisionError:
+                prompt(message('division_by_zero', user_lang))
+                result = "Calculation Error"
+
+    result = round(result, 2)
 
     # Display result
-    prompt(f'The result is {result}')
+    prompt(f'{num_1} {op_type} {num_2}  = {result}')
 
-    prompt(MESSAGES[user_lang]['another_operation'])
-    another_one = input()
-    
-    if (another_one and another_one[0].lower()) != 'y':
+    # Ask if user wants to do another calculation
+    prompt(message('another_operation', user_lang))
+    another_one = input().strip()
+
+    # Terminate if user enters anything other than 'yes' or 'y'
+    # If no input, assume "no"
+    if (another_one and another_one[0].lower()) != \
+    MESSAGES[user_lang]['yes'][0]:
         break
